26f17a89f19e7467f55ba245978fff5fecce4633
“Your <linssuchi@gmail.com>
2018-12-04 22:39:50 +0800

    Initial Commit

---
+++ 00457022_work2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  00457022_work2
+//
+//  Created by User01 on 2018/12/4.
+//  Copyright © 2018 00557146. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
